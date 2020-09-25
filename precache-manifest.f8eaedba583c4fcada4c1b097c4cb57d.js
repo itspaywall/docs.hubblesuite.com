d475dde1a73bdcad7aad17390c3fd2dbb22367c0
Joel E. Rego <joelerego@gmail.com>
2020-09-25 12:00:14 +0530

    Initial commit

---
+++ precache-manifest.f8eaedba583c4fcada4c1b097c4cb57d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ac910b6c0602efcebfdf70f5594883",
+    "url": "/index.html"
+  },
+  {
+    "revision": "731690f4b046e634d734",
+    "url": "/static/js/0.10b31dcb.chunk.js"
+  },
+  {
+    "revision": "e4a0d87becbcc6d09668875fe197b50f",
+    "url": "/static/js/0.10b31dcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d9e00ebf330dce9d4a",
+    "url": "/static/js/1.461639a5.chunk.js"
+  },
+  {
+    "revision": "5b8265f8ff73be70e6ea",
+    "url": "/static/js/2.2f9ec35b.chunk.js"
+  },
+  {
+    "revision": "83f213c82b2d702c08de",
+    "url": "/static/js/5.98f7f582.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/5.98f7f582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aae8e27100327436d08",
+    "url": "/static/js/6.458d69f4.chunk.js"
+  },
+  {
+    "revision": "7df8d19e4d3273d981da",
+    "url": "/static/js/7.b890c824.chunk.js"
+  },
+  {
+    "revision": "e52b20597e1d2102410b",
+    "url": "/static/js/8.85bd1167.chunk.js"
+  },
+  {
+    "revision": "71ee91dcb55164d9f90c",
+    "url": "/static/js/main.253de594.chunk.js"
+  },
+  {
+    "revision": "0ecf65971a809f71a728",
+    "url": "/static/js/runtime-main.bb934d5b.js"
+  },
+  {
+    "revision": "d90b3bff9221bde91893bbc7a5083756",
+    "url": "/static/media/Hubble.d90b3bff.ttf"
+  }
+]);
